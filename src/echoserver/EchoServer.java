fb2a294190db019ade82a8065bd7f4a18a6bc3d8
EthanUphoff <ethan.uphoff@gmail.com>
2018-11-06 10:42:45 -0600

    Finished Server, all tests work successfully.
    The server is now threaded.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.lang.*;
 
 public class EchoServer {
 	public static final int PORT_NUMBER = 6013;
@@ -17,12 +18,33 @@ public class EchoServer {
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 		while (true) {
+			//Connecting the serverSocket to the client socket
 			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int b;
-			while ((b = inputStream.read()) != -1) {
-				outputStream.write(b);
+			//Creates a thread with a clientHandler Runnable
+			Thread t = new Thread(new ClientHandler(socket));
+			//Starts Thread
+			t.start();
+		}
+	}
+	//ClientHandler implements the Runnable because it will be implemented in a Thread
+	public class ClientHandler implements Runnable{
+		Socket s;
+		//Constructor
+		public ClientHandler(Socket s){
+			this.s = s;
+		}
+		//Recieves input from the client and echos them back. Also Shuts down the client so that it doesnt run indefinitely
+		public void run(){
+			try{
+				InputStream inputStream = s.getInputStream();
+				OutputStream outputStream = s.getOutputStream();
+				int b;
+				while((b = inputStream.read()) != -1){
+					outputStream.write(b);
+				}
+				s.shutdownOutput();
+			} catch (IOException e){
+				System.err.println("Reached maximum amount of threads.");
 			}
 		}
 	}
